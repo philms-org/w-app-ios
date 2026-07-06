4c658c52b9340bd60a30c8dc29c21e5ffa960440
SR <sr@Mac.lan1>
2026-07-06 14:01:58 -0400

    fix: update productName to The W App in pbxproj

--- scripts/rebrand_target.rb
+++ scripts/rebrand_target.rb
@@ -10,6 +10,7 @@ project = Xcodeproj::Project.open(PROJECT_PATH)
 project.targets.each do |target|
   next unless target.name == OLD_NAME
   target.name = NEW_NAME
+  target.product_name = NEW_NAME
   target.build_configurations.each do |config|
     config.build_settings['PRODUCT_BUNDLE_IDENTIFIER'] = NEW_BUNDLE_ID
     config.build_settings['PRODUCT_NAME'] = NEW_NAME
