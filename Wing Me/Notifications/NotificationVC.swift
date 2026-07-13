d96af9358bae8e05bdfe44ffcc92e630cd3cd50a
SR <sr@Macbook-Pro-Santiago-Ramos.local>
2026-07-13 00:27:04 -0400

    feat: stub NotificationVC to empty state, remove PHP notifications call

--- Wing Me/Notifications/NotificationVC.swift
+++ Wing Me/Notifications/NotificationVC.swift
@@ -1,57 +1,33 @@
-
 import UIKit
 
-class NotificationVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
+class NotificationVC: UIViewController {
+
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var indicator: UIActivityIndicatorView!
-    
-    var array: [CustomCell] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        request()
+        view.backgroundColor = .systemBackground
+        tableView?.isHidden = true
+        indicator?.stopAnimating()
+        setupEmptyState()
     }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return array.count
+
+    private func setupEmptyState() {
+        let label = UILabel()
+        label.text = "No notifications yet."
+        label.textColor = .secondaryLabel
+        label.font = .systemFont(ofSize: 16)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label)
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        ])
     }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "NotificationCell", for: indexPath) as! NotificationCell
-        cell.updateCell(customCell: array[indexPath.row])
-        return cell
-    }
-    
+
     @IBAction func back(_ sender: UIButton) {
         dismiss(animated: true)
-    }
-    
-    func request() {
-        let path = "get_notification.php"
-        
-        let params: NSDictionary = [
-            "language": Strings.language
-        ]
-        
-        params.request(delegate: self, path: path, stopLoading: stopLoading, requestSuccess: requestSuccess)
-    }
-    
-    func stopLoading() {
-        indicator.stopAnimating()
-    }
-    
-    func requestSuccess(jsonObject: AnyObject) {
-        if let message = jsonObject["message"] as? [NSDictionary] {
-            for each in message {
-                let insert_at = each.getString(key: "insert_at")
-                let date: String = insert_at.getDate(format: "dd MMMM yyyy - hh:mm a")
-                
-                array.append(CustomCell.init(string1: each.getString(key: "title"),
-                                             string2: each.getString(key: "text"),
-                                             string3: date))
-            }
-            tableView.reloadData()
-        }
     }
 }
